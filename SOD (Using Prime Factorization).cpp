4ae8aee4fedb043f82b30e32a2684b7b24090b2a
Tarikul Islam <127613853+Tarik0011@users.noreply.github.com>
2023-03-16 14:07:55 +0600

    Create SOD (Using Prime Factorization).cpp

---
+++ SOD (Using Prime Factorization).cpp
@@ -0,0 +1,69 @@
+/*
+*   -------------- ?*?*? --------------
+* |         In The Name of *Allah*     |
+* |             Author : Tarik         |
+* |                                    |
+*  -------------- ?*?*? ---------------
+*/
+ 
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define  pb        push_back
+#define  all(v)    v.begin(),v.end()
+#define  endl       '\n'  
+#define  uint       long long
+#define  int        long long
+#define  fastio    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set; 
+
+const int mxn=1e7+123;
+bool is_prime[mxn];
+vector<int>primes;
+
+void sieve(int n){
+	for(int i=1;i<=n;i++)is_prime[i]=1;
+	for(int i=2;i<=n;i++){
+		if(is_prime[i]){
+			primes.push_back(i);
+			for(int j=i*i;j<=n;j+=i){
+				is_prime[j]=0;
+			}
+		}
+	}
+}
+int SOD(int n){
+
+   int ans=1;
+   for(auto x:primes){
+   	if(x*x>n)break;
+   	if(n%x==0){
+   		int pw=1,sum=1;
+   		while(n%x==0){
+   			pw*=x;
+   			sum+=pw;
+   			n/=x;
+   		}
+   		ans*=sum;
+   	}
+   
+   }
+   if(n>1)ans*=(n+1);
+   return ans;
+}
+void solve(){
+   int n;
+   cin>>n;
+   cout<<SOD(n)<<endl;
+   
+}
+signed main(){ 
+    fastio; 
+    sieve(1e7);
+    int t=1;
+    //cin>>t;
+    while(t--){
+       solve();
+    }
+}
