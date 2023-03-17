a2842ee7a36228fe5b690e5bcf09a40288b09675
Tarikul Islam <127613853+Tarik0011@users.noreply.github.com>
2023-03-17 18:27:18 +0600

    Update Essential Templete.cpp

--- কাপঝাপ/Essential Templete.cpp
+++ কাপঝাপ/Essential Templete.cpp
@@ -1,26 +1,20 @@
-/*
-*   -------------- ?*?*? --------------
-* |         In The Name of *Allah*     |
-* |             Author : Tarik         |
-* |                                    |
-*  -------------- ?*?*? ---------------
-*/
- 
 #include<bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-using namespace std;
-#define  pb        push_back
-#define  all(v)    v.begin(),v.end()
-#define  endl       '\n'  
-#define  uint       long long
-#define  int        long long
-#define  fastio    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
-typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set; 
+all comperator function{
+/*
+// priority_queue<pair<int,int>,cmp>pq;
+// set<pair<int,int>,cmp>s;
+// cmp function
+struct cmp{
+	constexpr bool operator()(pair<int,int>const& a,pair<int,int>const&b)
+              const noexcept{
+		if(a.first==b.first)return a.second>b.second;
+               	return a.first<b.first;
+	}
+};
 
-const int mxn=1e6+123;
-int a[mxn];
 
+}
 void solve(){
  /*
 	multiset<pair<int,int>>ms;
