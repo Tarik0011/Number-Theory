1ddd1e44e35013c9db77be4a78bb7b34eb7afa6f
Tarikul Islam <127613853+Tarik0011@users.noreply.github.com>
2023-11-25 19:02:47 +0600

    Update MEX with array updates.cpp

--- কাপঝাপ/MEX with array updates.cpp
+++ কাপঝাপ/MEX with array updates.cpp
@@ -78,3 +78,67 @@ signed main(){
        solve();
     }
 }
+
+
+/*
+*   -------------- ?*?*? --------------
+* |         In The Name of *Allah*     |
+* |             Author : Tarik         |
+* |                                    |
+*  -------------- ?*?*? ---------------
+*/
+ /*
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define  pb        push_back
+#define  all(v)    v.begin(),v.end()
+#define  endl       '\n'  
+#define  uint       long long
+#define  int        long long
+#define  fastio    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set; 
+ 
+const int mxn=2e6+123;
+int a[mxn];
+void solve(){
+	int n,q;
+	cin>>n>>q;
+	map<int,int>fre;
+	set<int>s;
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+		fre[a[i]]++;
+	}
+	for(int i=0;i<=n;i++){
+		s.insert(i);
+	}
+	for(int i=1;i<=n;i++){
+		s.erase(a[i]);
+	}
+	
+	while(q--){
+		int idx,val;
+		cin>>idx>>val;
+		fre[a[idx]]--;
+		if(fre[a[idx]]==0){
+			s.insert(a[idx]);
+		}
+		a[idx]=val;
+		fre[val]++;
+		s.erase(val);
+		cout<<*s.begin()<<endl;
+	}
+
+}
+signed main(){ 
+    fastio; 
+    int t=1; 
+    //cin>>t;
+    for(int i=1;i<=t;i++){
+       //cout<<"Case "<<i<<": ";
+       solve();
+    }
+}
+*/
